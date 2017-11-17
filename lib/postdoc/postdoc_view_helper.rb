6cd16266f72874680dd10711351b61f2b07f41fd
Frank Groeneveld <frank@ivaldi.nl>
2017-11-17 16:31:07 +0100

    Support application specific css

--- lib/postdoc/postdoc_view_helper.rb
+++ lib/postdoc/postdoc_view_helper.rb
@@ -1,12 +1,20 @@
 module PostdocViewHelper
-  def postdoc_default_styles
+  # outputs style tag with default styling such as A4 paper format
+  def postdoc_default_stylesheet_link_tag
     content_tag :style,
         File.read(postdoc_gem_asset('stylesheets', 'default.css')),
         type: 'text/css'
   end
 
+  def postdoc_stylesheet_link_tag(*path)
+    content_tag :style,
+        File.read(Rails.root.join('app', 'assets', 'stylesheets', *path)),
+        type: 'text/css'
+  end
+
   protected
 
+  # include an assets from the gem app/assets folder
   def postdoc_gem_asset(*path)
     File.join(File.dirname(__FILE__), '..', '..', 'app', 'assets', *path)
   end
