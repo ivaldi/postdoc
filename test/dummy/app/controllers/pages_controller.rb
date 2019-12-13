29f3f91e550979481c69e85e0f79adff2649a6d7
Frank Groeneveld <frank@ivaldi.nl>
2019-12-13 17:05:47 +0100

    Extend examples

--- test/dummy/app/controllers/pages_controller.rb
+++ test/dummy/app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def show
-    render pdf: 'show'
+    render pdf: 'show',
+        header_template: File.read(Rails.root.join('app', 'views', 'pages', 'header.html')),
+        footer_template: File.read(Rails.root.join('app', 'views', 'pages', 'footer.html'))
   end
 end
