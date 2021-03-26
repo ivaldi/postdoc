57e0fe26de8b9f40d72266acbc278786793550be
Bas Schoenmakers <bas@ivaldi.nl>
2021-03-26 11:34:52 +0100

    catch wider to prevent specific errors

--- lib/postdoc/client.rb
+++ lib/postdoc/client.rb
@@ -41,7 +41,7 @@ module Postdoc
     def setup_connection_or_wait
       @client = ChromeRemote.client(port: @port)
       true
-    rescue Errno::EHOSTUNREACH, Errno::ECONNREFUSED
+    rescue Exception
       sleep(0.1)
       false
     end
